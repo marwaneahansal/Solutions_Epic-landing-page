f0e60b1e5aeb726b5278a6c5275807647053212c
marwane ahansal <ahansalmarwane@gmail.com>
2020-09-22 23:24:48 +0100

    male the site responsive

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('scroll', () => {
     if(window.pageYOffset > (document.getElementById("aboutUs").offsetTop - 100)) {
         document.querySelector('.navbar').classList.add('fixed');
@@ -16,26 +17,52 @@ gsap.to('#loading', 3, {
     }
 });
 
-gsap.from(".homeSection", 3, {
-    delay: 2.3,
-    opacity: 0,
-    y: 50,
-    ease: Expo.easeInOut
-});
+// gsap.from(".homeSection", 3, {
+//     delay: 2.3,
+//     opacity: 0,
+//     y: 50,
+//     ease: Expo.easeInOut
+// });
 
 
-gsap.from(".logo", 3, {
-    delay: 2.2,
-    opacity: 0,
-    y: -100,
-    ease: Expo.easeInOut
-});
+// gsap.from(".logo", 3, {
+//     delay: 2.2,
+//     opacity: 0,
+//     y: -100,
+//     ease: Expo.easeInOut
+// });
 
 
-gsap.from("nav ul", 3, {
-    delay: 2.2,
-    opacity: 0,
-    y: -100,
-    ease: Expo.easeInOut
-});
+// gsap.from("nav ul", 3, {
+//     delay: 2.2,
+//     opacity: 0,
+//     y: -100,
+//     ease: Expo.easeInOut
+// });
 
+
+
+
+
+
+const navSlide = () => {
+    const menuToggle = document.querySelector('.menuToggle');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        menuToggle.classList.toggle('toggle');
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = "";
+            } else link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        });
+    });
+
+    
+}
+
+navSlide();
