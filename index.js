327d71bfd725dd1d0475fdcee8b6891ea4f215cc
marwane ahansal <ahansalmarwane@gmail.com>
2020-09-23 11:08:03 +0100

    finish this project

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('scroll', () => {
     if(window.pageYOffset > (document.getElementById("aboutUs").offsetTop - 100)) {
         document.querySelector('.navbar').classList.add('fixed');
@@ -8,37 +7,41 @@ document.addEventListener('scroll', () => {
         document.querySelector('.navbar').classList.remove('fixed');
     }
 });
-gsap.to('#loading', 3, {
-    delay: 2,
-    top: "-110%",
-    ease: Expo.easeInOut,
-    onComplete: () => {
-        document.getElementById('landing').classList.remove('anime');
-    }
-});
-
-// gsap.from(".homeSection", 3, {
-//     delay: 2.3,
-//     opacity: 0,
-//     y: 50,
-//     ease: Expo.easeInOut
-// });
-
-
-// gsap.from(".logo", 3, {
-//     delay: 2.2,
-//     opacity: 0,
-//     y: -100,
-//     ease: Expo.easeInOut
-// });
-
-
-// gsap.from("nav ul", 3, {
-//     delay: 2.2,
-//     opacity: 0,
-//     y: -100,
-//     ease: Expo.easeInOut
-// });
+if(document.body.offsetWidth >= 900) {
+    console.log("test");
+    gsap.to('#loading', 3, {
+        delay: 2,
+        top: "-110%",
+        ease: Expo.easeInOut,
+        onComplete: () => {
+            document.getElementById('landing').classList.remove('anime');
+        }
+    });
+    
+    gsap.from(".homeSection", 3, {
+        delay: 2.3,
+        opacity: 0,
+        y: 50,
+        ease: Expo.easeInOut
+    });
+    
+    
+    gsap.from(".logo", 3, {
+        delay: 2.2,
+        opacity: 0,
+        y: -100,
+        ease: Expo.easeInOut
+    });
+    
+    
+    gsap.from("nav ul", 3, {
+        delay: 2.2,
+        opacity: 0,
+        y: -100,
+        ease: Expo.easeInOut
+    });
+    
+}
 
 
 
